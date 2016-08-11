88f649c12ce3fbabd25d05628a813fb405fa58ca
Bryon Nicoson <bryon.nicoson@gmail.com>
2016-08-11 15:23:27 -0500

    added body to increase duration to activate servo

--- test.rb
+++ test.rb
@@ -3,14 +3,16 @@ require 'bundler'
 Bundler.require()
 
 headers = {
-	"Authorization" => "Bearer [INSERT API KEY HERE]",
+	"Authorization" => "Bearer INSERT_YOUR_KEY_HERE",
 	"Accept" => "application/vnd.littlebits.v2+json",
 	"Content-Type" => "application/json"
 }
+body = {"percent" => 100, "duration_ms" => 2000}
 
 1.upto(5) do
-	HTTParty.post("https://api-http.littlebitscloud.cc/devices/00e04c1f0376/output",
-		:headers => headers 
+	HTTParty.post("https://api-http.littlebitscloud.cc/devices/INSERT_DEVICE_ID_HERE/output",
+		:headers => headers,
+		:body => body.to_json
 	)
-	sleep 10
+	sleep 5
 end
